b581a24cd97aff41f030f0c826745dc916f8e03c
MaryLila <maria.gruzdova@gmail.com>
2024-04-06 12:48:28 +0200

    Capitalize

--- str_func.py
+++ str_func.py
@@ -1,2 +1,6 @@
-def uppercase_string(s):
-    return s.upper()
+def capitalize_words(s):
+    """
+    Capitalize the first letter of each word in a string.
+    """
+    return ' '.join(word.capitalize() for word in s.split())
+
